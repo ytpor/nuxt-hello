f0a3c0976982f99152694fbd5ff140d3c34a7bc6
YT Por <agtong+ytpor@gmail.com>
2025-12-04 14:28:34 +0800

    first commit

---
+++ app/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    hello
+  </div>
+</template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+  text-align: center;
+}
+</style>
